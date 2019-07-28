39a18624b020da02b902c66a17a14bea383cc3dd
rico.putra <rico.putra@outlook.co.id>
2019-07-28 20:54:10 +0700

    [UPDATE] Implement Delete item func

--- app.py
+++ app.py
@@ -84,3 +84,13 @@ def update_todo(id):
     db.session.commit()
 
     return todo_schema.jsonify(todo_old)
+
+
+# Delete Todo
+@app.route('/todo/<id>', methods=['DELETE'])
+def delete_todo(id):
+    todo = Todo.query.get(id)
+    db.session.delete(todo)
+    db.session.commit()
+
+    return todo_schema.jsonify(todo)
